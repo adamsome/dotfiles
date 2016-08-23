578f4250aff0a0b619cb7a6657680fcb161e7d66
Adam Some <adamsome@gmail.com>
2016-08-23 13:43:22 -0400

    Add checks for mac v. babun

--- macos/install.sh
+++ macos/install.sh
@@ -3,5 +3,9 @@
 # command line interface to it that we can use to just install everything, so
 # yeah, let's do that.
 
-echo "› sudo softwareupdate -i -a"
-sudo softwareupdate -i -a
+# If we're on a Mac
+if [ "$(uname -s)" == "Darwin" ]
+then
+  echo "› sudo softwareupdate -i -a"
+  sudo softwareupdate -i -a
+fi
