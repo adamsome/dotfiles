bfe01b8007bc7237b535d0e0d7a1030486c71917
Adam Some <adamsome@gmail.com>
2016-09-20 23:58:28 -0400

    Merge major vimrc changes from remote

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Ask for the administrator password upfront
-sudo -v
+#sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until `.macos` has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+#while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 ###############################################################################
 # General UI/UX                                                               #
@@ -17,7 +17,8 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 #sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "0x6D746873"
 
 # Set standby delay to 24 hours (default is 1 hour)
-sudo pmset -a standbydelay 86400
+#sudo pmset -a standbydelay 86400
+pmset -a standbydelay 86400
 
 # Disable the sound effects on boot
 #sudo nvram SystemAudioVolume=" "
