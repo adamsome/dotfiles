1188db97189e33db3e4104d63c277ea56a8d07a0
Adam Some <adamsome@gmail.com>
2018-09-04 23:53:14 -0400

    Remove vim incsearch & add yt alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,6 @@ alias reload!='. ~/.zshrc'
 alias cls='clear' # Good 'ol Clear Screen command
 
 alias v='nvim'
+
+alias ytdj="youtube-dl -x --no-mtime --audio-format mp3 --audio-quality 0 -o '/Users/Adam/Music/DJ-Drop/$(date +%Y-%m-%d) %(title)s [%(id)s].%(ext)s'"
+# alias ytm='youtube-dl -x --no-mtime --audio-format mp3 --audio-quality 0 -o ~/Music/DJ-Drop/$(date +%Y%m%d)-%(title)s-%(id)s.%(ext)s\'''
